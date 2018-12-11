ca26c704e664f385d5bb72e13624ab6b611ff4b9
hendikajulbahri <julbahrih@gmail.com>
2018-12-11 15:19:21 +0700

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state={
+      gambarprofil:'',
+      inputnama:''
+    }
+  }
+
   render() {
+
+    let klik = (e)=>{
+
+    
+      // let nama= document.getElementById('inputan').value
+    
+      this.setState({
+        gambarprofil: `https://robohash.org/${this.state.inputnama}`
+      })
+    }
+  
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="container-fluid grid">
+      <div class="row">
+          <div class="col-12 text-center">
+
+              <center>
+
+              </center>
+          </div>
+      </div>
+
+      <div class="row ">
+          <div class="col-12 p-0  text-center">
+          <input id="inputan" type="text" onChange={(e)=>{this.setState({inputnama: e.target.value})}}></input>
+          <br/>
+          <br/>
+          <button className="btn btn-success" onClick={klik}>Klik</button>
+          <div className="gambar">
+       <img className="satu" src={this.state.gambarprofil}/>
+       </div>
+
+          </div>
+
+        
+</div>
       </div>
     );
   }
